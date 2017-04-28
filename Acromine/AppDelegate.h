3487879fc4e7720f5a8691a9df77a80e1fffd5eb
Mike Cohen <mike3k@gmail.com>
2017-04-28 10:27:32 -0700

    Initial Commit

---
+++ Acromine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acromine
+//
+//  Created by Mike Cohen on 4/28/17.
+//  Copyright © 2017 Mike Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acromine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Acromine
+//
+//  Created by Mike Cohen on 4/28/17.
+//  Copyright © 2017 Mike Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
